2c887dcc9a9ea1e3161e2632dfb1f2ff22a3c2be
Xelton <129615003+XeltonOsu@users.noreply.github.com>
2023-04-01 21:57:01 +0500

    Add files via upload

---
+++ Configuration.py
@@ -0,0 +1,12 @@
+
+# DisableNagle: reduce delay and improve performance between client and server BUT require more power #
+
+settings = {
+    "DisableNagle": True,
+    "PrintEnable": True,
+    "UseContentUpdater": False,
+    "Proxy": False,
+    "DumpPacket": False,
+    "BlackList": [24109],
+    "DumpMajor": 44
+}
